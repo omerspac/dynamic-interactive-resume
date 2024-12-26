739173f0b751f6281d998fa41e08ca11f18f0acd
omerspac <omerspac@gmail.com>
2024-12-26 16:02:25 +0500

    c8

--- form.js
+++ form.js
@@ -11,3 +11,51 @@ document.getElementById('resumeForm').addEventListener('submit', (event) => {
 
     window.location.href = 'resume.html';
 });
+
+let workIndex = 1;
+let educationIndex = 1;
+
+document.getElementById('addWorkExperience').addEventListener('click', () => {
+    const workSection = document.getElementById('workExperienceSection');
+    const newWorkEntry = document.createElement('div');
+    newWorkEntry.id = `workExperience${workIndex}`;
+    newWorkEntry.classList.add('work-entry');
+    
+    newWorkEntry.innerHTML = `
+        <hr /><br>
+        <label for="workTitle${workIndex}" class="form-titles">Work Title:</label><br>
+        <input type="text" id="workTitle${workIndex}" name="workTitle${workIndex}" class="form-textbox" placeholder="e.g., Graphics Designer" required><br><br>
+
+        <label for="workPlace${workIndex}" class="form-titles">Workplace:</label><br>
+        <input type="text" id="workPlace${workIndex}" name="workPlace${workIndex}" class="form-textbox" placeholder="e.g., Freelance/Projects" required><br><br>
+
+        <label for="workDate${workIndex}" class="form-titles">Work Date:</label><br>
+        <input type="text" id="workDate${workIndex}" name="workDate${workIndex}" class="form-textbox" placeholder="e.g., Aug 2022 - Present" required><br><br>
+
+        <label for="workDescription${workIndex}" class="form-titles">Work Description:</label><br>
+        <textarea id="workDescription${workIndex}" name="workDescription${workIndex}" placeholder="Describe your responsibilities and achievements" class="work-description" required></textarea><br><br>
+    `;
+    workSection.insertBefore(newWorkEntry, document.getElementById('addWorkExperience'));
+    workIndex++;
+});
+
+document.getElementById('addEducation').addEventListener('click', () => {
+    const educationSection = document.getElementById('educationSection');
+    const newEducationEntry = document.createElement('div');
+    newEducationEntry.id = `education${educationIndex}`;
+    newEducationEntry.classList.add('education-entry');
+
+    newEducationEntry.innerHTML = `
+        <hr /><br>
+        <label for="degree${educationIndex}" class="form-titles">Degree/Major:</label><br>
+        <input type="text" id="degree${educationIndex}" name="degree${educationIndex}" class="form-textbox" placeholder="e.g., Bachelors" required><br><br>
+
+        <label for="university${educationIndex}" class="form-titles">Education Place:</label><br>
+        <input type="text" id="university${educationIndex}" name="university${educationIndex}" class="form-textbox" placeholder="e.g., XYZ University" required><br><br>
+
+        <label for="educationDate${educationIndex}" class="form-titles">Start Date - End Date:</label><br>
+        <input type="text" id="educationDate${educationIndex}" name="educationDate${educationIndex}" class="form-textbox" placeholder="e.g., Sep 2024 - Sep 2028 (In progress)" required><br><br>
+    `;
+    educationSection.insertBefore(newEducationEntry, document.getElementById('addEducation'));
+    educationIndex++;
+});
--- resume.js
+++ resume.js
@@ -7,8 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
             <head>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
             </head>
+            <br>
+            <img src="${resumeData.pfpLink}" class="profile-pic">
             <p class="resume-name"><strong>${resumeData.fullName}</strong></p>
-            <p class="resume-job-title"><strong>${resumeData.jobTitle}</strong></p>
+            <p class="resume-job-title">${resumeData.jobTitle}</p>
             <br>
 
             <header>
@@ -20,35 +22,56 @@ document.addEventListener('DOMContentLoaded', () => {
             <section>
                 <p class="resume-work-content">${resumeData.personalInfo}</p>
             </section>
+        `;
 
+        if (resumeData.workTitle0) {
+            resumeContent.innerHTML += `
+                <header>
+                    <hr>
+                    <p class="titles" align="center"><b>WORK EXPERIENCE</b></p>
+                    <hr>
+                </header>
+            `;
+            let workExperienceHTML = '';
+            let workIndex = 0;
+            while (resumeData[`workTitle${workIndex}`]) {
+                workExperienceHTML += `
+                    <section>
+                        <p class="work-titles"><i class="fas fa-arrow-right"></i> ${resumeData[`workTitle${workIndex}`]}</p>
+                        <p class="work-wheres">${resumeData[`workPlace${workIndex}`]}</p>
+                        <p class="work-time">${resumeData[`workDate${workIndex}`]}</p>
+                        <p class="work-content">${resumeData[`workDescription${workIndex}`]}</p>
+                    </section><br><hr>
+                `;
+                workIndex++;
+            }
+            resumeContent.innerHTML += workExperienceHTML;
+        }
+
+        if (resumeData.degree0) {
+            resumeContent.innerHTML += `
+                <header>
+                    <p class="titles" align="center"><b>ACADEMICS & EDUCATION</b></p>
+                    <hr>
+                </header>
+            `;
+            let educationHTML = '';
+            let educationIndex = 0;
+            while (resumeData[`degree${educationIndex}`]) {
+                educationHTML += `
+                    <section>
+                        <p class="work-titles"><i class="fas fa-arrow-right"></i> ${resumeData[`degree${educationIndex}`]}</p>
+                        <p class="work-wheres">${resumeData[`university${educationIndex}`]}</p>
+                        <p class="work-time">${resumeData[`educationDate${educationIndex}`]}</p>
+                    </section><br><hr>
+                `;
+                educationIndex++;
+            }
+            resumeContent.innerHTML += educationHTML;
+        }
+
+        resumeContent.innerHTML += `
             <header>
-                <hr>
-                <p class="titles" align="center"><b>WORK EXPERIENCE</b></p>
-                <hr>
-            </header>
-
-            <section>
-                <p class="work-titles"><i class="fas fa-arrow-right"></i> ${resumeData.workTitle}</p>
-                <p class="work-wheres">${resumeData.workPlace}</p>
-                <p class="work-time">${resumeData.workDate}</p>
-
-                <p class="work-content">${resumeData.workDescription}</p>
-            </section><br>
-
-            <header>
-                <hr>
-                <p class="titles" align="center"><b>ACADEMICS & EDUCATION</b></p>
-                <hr>
-            </header>
-
-            <section>
-                <p class="work-titles"><i class="fas fa-arrow-right"></i> ${resumeData.degree}</p>
-                <p class="work-wheres">${resumeData.university}</p>
-                <p class="work-time">${resumeData.educationDate}</p>
-            </section>
-
-            <header>
-                <hr>
                 <p class="titles" align="center"><b>KNOWLEDGE & SKILLS</b></p>
                 <hr>
             </header>
@@ -56,7 +79,9 @@ document.addEventListener('DOMContentLoaded', () => {
             <section>
                 <p class="resume-work-content">${resumeData.skills}</p>
             </section>
+        `;
 
+        resumeContent.innerHTML += `
             <header>
                 <hr>
                 <p class="titles" align="center"><b>SOCIAL CONTACTS</b></p>
@@ -75,8 +100,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 </ul>
             </section>
         `;
-
     } else {
         resumeContent.innerHTML = `<p class="resume-empty-message">No data available. Please go back and fill the form.</p>`;
     }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const generateResumeButton = document.getElementById('generateResumeButton');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 200) {
+            generateResumeButton.style.display = 'block';
+        } else {
+            generateResumeButton.style.display = 'none';
+        }
+    });
 });
